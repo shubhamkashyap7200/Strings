00c854e04213c476450f0a2be03544c78903bfa2
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2025-01-08 11:04:06 +0530

    Practise

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -19,4 +19,40 @@ print(characterIneraction.lowercased())
 print(characterIneraction.hasPrefix("I am fine"))
 print(characterIneraction.hasSuffix("children"))
 
+// Multiline Strings
+let myMotto = "The path of perfection must go through the pain of practise"
+let userErrorAlert = """
+Error
+Look like something went wrong!
+Kindly retry again
+"""
 
+print(myMotto)
+print(userErrorAlert)
+
+
+// Ints
+let basketballShots = 5
+var basketballShotsScore = 5
+
+let myRetirementMoney = 5_000_000
+
+print(myRetirementMoney)
+
+let loweredScore = basketballShotsScore - 1
+let upperScore = basketballShotsScore + 2
+let doubleScore = basketballShotsScore * 2
+let halvedScore = basketballShotsScore % 2
+
+print(halvedScore)
+
+basketballShotsScore += 10 // Compound assignment operator
+print(basketballShotsScore)
+
+
+let factor = 1256
+print(factor.isMultiple(of: 4))
+
+// Float and Double
+let newNumber = 1 + 3.0
+print(type(of:newNumber))
